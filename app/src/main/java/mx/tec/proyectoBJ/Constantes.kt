5353a9c490fa5d1ec193deb455efa5eb32dd4ac3
Milllll000 <156021494+Milllll000@users.noreply.github.com>
2025-10-10 16:40:24 -0600

    Agregar nuevos colores y actualizar PPNegocio
    Introduced 'negro' and 'grisClaro' colors in Constantes.kt. Refactored PantallaPrincipalNegocio to use Scaffold with top and bottom bars, and added composables for displaying business name and description with updated styling.

--- app/src/main/java/mx/tec/proyectoBJ/Constantes.kt
+++ app/src/main/java/mx/tec/proyectoBJ/Constantes.kt
@@ -14,5 +14,7 @@ val morado = Color(0xFF38156E)
 val rosa = Color(red = 243, green = 26, blue = 138, alpha = 255)
 val naranja = Color(red = 250, green = 77, blue = 103, alpha = 255)
 val blanco = Color(0xFFFFFFFF)
+val negro = Color(0xFF000000)
+val grisClaro = Color(0xFFE0E0E0)
 
 val degradado = Brush.horizontalGradient(listOf(rosa, naranja))
--- app/src/main/java/mx/tec/proyectoBJ/view/PantallaPrincipalNegocio.kt
+++ app/src/main/java/mx/tec/proyectoBJ/view/PantallaPrincipalNegocio.kt
@@ -1,9 +1,64 @@
 package mx.tec.proyectoBJ.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import mx.tec.ptoyectobj.grisClaro
+import mx.tec.ptoyectobj.morado
+import mx.tec.ptoyectobj.negro
 
 @Composable
 fun PantallaPrincipalNegocio(modifier: Modifier = Modifier) {
+
+    Scaffold(
+        topBar = { BarraSuperior() },
+        bottomBar = { BarraInferior() }
+    ) { innerPadding ->
+        Box(modifier = modifier.background(morado)
+            .padding(innerPadding)) {
+            NegocioNombre(nombre = "JÖTUNHEIM", descripcion = "Academia de Artes Marciales") 
+        }
+    }
+
+}
+
+@Composable
+fun NegocioNombre(modifier: Modifier = Modifier,
+                  nombre: String,
+                  descripcion: String) {
+    Text(nombre,
+        modifier = modifier,
+        style = TextStyle(
+            color = negro,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold),
+    )
+    Text(descripcion,
+        modifier = modifier,
+        style = TextStyle(
+            color = grisClaro,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center
+        )
+    )
+}
+
+@Composable
+fun BarraSuperior(modifier: Modifier = Modifier) {
+    
+}
+
+@Composable
+fun BarraInferior(modifier: Modifier = Modifier) {
     
 }
