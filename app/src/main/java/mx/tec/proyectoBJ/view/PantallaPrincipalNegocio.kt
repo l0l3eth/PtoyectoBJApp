6d0f5859de2373ae272d363abe6b2e0a7ed238b8
Milllll000 <156021494+Milllll000@users.noreply.github.com>
2025-10-10 15:57:17 -0600

    Crear PantallaPrincipalNegocio.kt

---
+++ app/src/main/java/mx/tec/proyectoBJ/view/PantallaPrincipalNegocio.kt
@@ -0,0 +1,9 @@
+package mx.tec.proyectoBJ.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PantallaPrincipalNegocio(modifier: Modifier = Modifier) {
+    
+}
